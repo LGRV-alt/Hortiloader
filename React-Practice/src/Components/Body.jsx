7f16577097d5bc9412a3caeb527f2b2d4d72dd43
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2024-07-20 22:33:10 +0100

    Update Body.jsx

--- React-Practice/src/Components/Body.jsx
+++ React-Practice/src/Components/Body.jsx
@@ -5,7 +5,7 @@ function Body() {
         <h2 className="text-2xl text-black  bg-blue-500 flex justify-center items-center ">
           Im the body component
         </h2>
-        <h2 className="text-2xl text-black  bg-green-500 flex justify-center items-center col-start-2 row-start-1 row-end-3 ">
+        <h2 className="text-2xl text-black  bg-green-500 flex justify-center items-center col-start-2 row-start-1 row-end-3">
           Im the body component
         </h2>
         <h2 className="text-2xl text-black  bg-blue-500 flex justify-center items-center ">
