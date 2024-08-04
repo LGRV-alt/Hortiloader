8276c53b8f5c5248da5e3005b495596870dcafbb
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2024-08-04 21:16:41 +0100

    added current week to the header

--- React-Practice/src/Components/Header.jsx
+++ React-Practice/src/Components/Header.jsx
@@ -1,13 +1,36 @@
 function Header() {
+  function getDateWeek(date) {
+    const currentDate = typeof date === "object" ? date : new Date();
+    const januaryFirst = new Date(currentDate.getFullYear(), 0, 1);
+    const daysToNextMonday =
+      januaryFirst.getDay() === 1 ? 0 : (7 - januaryFirst.getDay()) % 7;
+    const nextMonday = new Date(
+      currentDate.getFullYear(),
+      0,
+      januaryFirst.getDate() + daysToNextMonday
+    );
+
+    return currentDate < nextMonday
+      ? 52
+      : currentDate > nextMonday
+      ? Math.ceil((currentDate - nextMonday) / (24 * 3600 * 1000) / 7)
+      : 1;
+  }
+
+  const currentDate = new Date();
+  const weekNumber = getDateWeek();
+
+  console.log("Week number of " + currentDate + " is : " + weekNumber);
   return (
     <>
-      <h2
+      <div
         className={
-          " flex justify-start items-center text-2xl pl-10 border-b-2 border-black mt-2"
+          " flex justify-between items-center  pl-10 border-b-2 border-black mt-2"
         }
       >
-        Hortiloader
-      </h2>
+        <h2 className="text-2xl">Hortiloader</h2>
+        <p className="pr-6 ">Current Week - {weekNumber}</p>
+      </div>
     </>
   );
 }
