b7ad0f77fdac92d8f04ff1ae5a3e55b8b232996f
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2024-08-09 17:43:45 +0100

    not sure

--- React-Practice/src/App.jsx
+++ React-Practice/src/App.jsx
@@ -4,9 +4,17 @@ import Footer from "./Components/Footer";
 import PocketBase from "pocketbase";
 import { useEffect, useState } from "react";
 import Login from "./Login";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 export default function App() {
   const [rec, setRecords] = useState([]);
+  const queryClient = new QueryClient();
   useEffect(() => {
     async function fetchData() {
       const pb = new PocketBase("https://hortiloader.pockethost.io");
@@ -19,12 +27,14 @@ export default function App() {
 
   return (
     <>
-      <div className="grid-cols-1 grid-rows-[1fr,10fr,0.5fr] grid w-screen h-dvh overflow-x-hidden">
-        <Header></Header>
-        {/* <Body records={rec}></Body> */}
-        <Login></Login>
-        <Footer></Footer>
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div className="grid-cols-1 grid-rows-[1fr,10fr,0.5fr] grid w-screen h-dvh overflow-x-hidden">
+          <Header></Header>
+          {/* <Body records={rec}></Body> */}
+          <Login></Login>
+          <Footer></Footer>
+        </div>
+      </QueryClientProvider>
     </>
   );
 }
