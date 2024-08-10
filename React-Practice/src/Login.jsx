eb65d31d553d9d38ded9111c57e713557e6746ee
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2024-08-10 21:06:54 +0100

    changes to the layout on login

--- React-Practice/src/Login.jsx
+++ React-Practice/src/Login.jsx
@@ -21,62 +21,73 @@ export default function Login() {
     signup(username, password);
   };
   return (
-    <div>
-      <div>
-        <h2>Log In As Existing User</h2>
-        <div className="grid gap-6 mt-4 text-base">
-          <input
-            className="text-input "
-            type="text"
-            placeholder="Username"
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-          <input
-            className="text-input"
-            onChange={(e) => setPassword(e.target.value)}
-            type="password"
-            placeholder="Password"
-          />
+    <div className="grid grid-rows-1 grid-cols-2">
+      <div className="flex flex-col justify-evenly items-center">
+        <div className="bg-white flex flex-col justify-center items-start">
+          <h2>Log In As Existing User</h2>
+          <div className="grid gap-6 mt-4 text-base">
+            <input
+              className="text-input "
+              type="text"
+              placeholder="Username"
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+            <input
+              className="text-input"
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              placeholder="Password"
+            />
+          </div>
+
+          <button
+            className="bg-green-500 text-white py-2 px-4 rounded-md text-base mt-6 hover:bg-green-600"
+            onClick={handleLogin}
+          >
+            <div className="flex">
+              <span className="material-symbols-outlined -ml-2">login</span>
+              <p className="text-base ml-2">Continue</p>
+            </div>
+          </button>
         </div>
 
-        <button
-          className="bg-green-500 text-white py-2 px-4 rounded-md text-base mt-6 hover:bg-green-600"
-          onClick={handleLogin}
-        >
-          <div className="flex">
-            <span className="material-symbols-outlined -ml-2">login</span>
-            <p className="text-base ml-2">Continue</p>
+        <div className="bg-white flex flex-col justify-center items-start">
+          <h2>Create A New Account</h2>
+          <div className="grid gap-6 mt-4 text-base">
+            <input
+              className="text-input "
+              type="text"
+              placeholder="Username"
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+            <input
+              className="text-input"
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              placeholder="Password"
+            />
           </div>
-        </button>
+
+          <button
+            className="bg-green-500 text-white py-2 px-4 rounded-md text-base mt-6 hover:bg-green-600"
+            onClick={handleSignup}
+          >
+            <div className="flex">
+              <span className="material-symbols-outlined -ml-2">login</span>
+              <p className="text-base ml-2">Continue</p>
+            </div>
+          </button>
+        </div>
       </div>
-      <div>
-        <h2>Create A New Account</h2>
-        <div className="grid gap-6 mt-4 text-base">
-          <input
-            className="text-input "
-            type="text"
-            placeholder="Username"
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-          <input
-            className="text-input"
-            onChange={(e) => setPassword(e.target.value)}
-            type="password"
-            placeholder="Password"
-          />
-        </div>
-
-        <button
-          className="bg-green-500 text-white py-2 px-4 rounded-md text-base mt-6 hover:bg-green-600"
-          onClick={handleSignup}
-        >
-          <div className="flex">
-            <span className="material-symbols-outlined -ml-2">login</span>
-            <p className="text-base ml-2">Continue</p>
-          </div>
-        </button>
+      <div className="bg-stone-500 flex items-center p-20">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+          quasi consequuntur quod saepe numquam facilis dicta quas expedita odit
+          libero, dolores deleniti dolor in at dignissimos ipsum necessitatibus
+          molestiae tenetur.
+        </p>
       </div>
     </div>
   );
