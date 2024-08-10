6236ce2cd7c39dce4341e83303131500251974bb
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2024-08-10 22:06:35 +0100

    removed data from test

--- React-Practice/src/templates/Contact.jsx
+++ React-Practice/src/templates/Contact.jsx
@@ -4,13 +4,6 @@ export default function Contact() {
       <h1>
         <center>
           <p>Contact Me.</p>
-          <a
-            href="https://github.com/swarajgosavi"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Swaraj Gosavi (GitHub) [@swarajgosavi]
-          </a>
         </center>
       </h1>
     </div>
