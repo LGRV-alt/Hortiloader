e3996fd2ee8b7067b88c5cfef6db52ce1c4bd538
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2024-08-10 19:53:32 +0100

    adding signup to the page

--- React-Practice/src/Login.jsx
+++ React-Practice/src/Login.jsx
@@ -1,46 +1,83 @@
 import { useState } from "react";
-import { login } from "./Components/lib/pocketbase";
+import { login, signup } from "./Components/lib/pocketbase";
 
 export default function Login() {
   const [username, setUsername] = useState(null);
   const [password, setPassword] = useState(null);
 
-  const handleSubmit = () => {
+  const handleLogin = () => {
     if (!username || !password) {
       window.alert("Invalid Login credentials");
       return;
     }
-
     login(username, password);
   };
+  const handleSignup = () => {
+    if (!username || !password) {
+      window.alert("Invalid Login credentials");
+      return;
+    }
+    console.log(username, password);
+    signup(username, password);
+  };
   return (
-    <>
-      <h2>Log In As Existing User</h2>
-      <div className="grid gap-6 mt-4 text-base">
-        <input
-          className="text-input "
-          type="text"
-          placeholder="Username"
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <input
-          className="text-input"
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          placeholder="Password"
-        />
-      </div>
-
-      <button
-        className="bg-green-500 text-white py-2 px-4 rounded-md text-base mt-6 hover:bg-green-600"
-        onClick={handleSubmit}
-      >
-        <div className="flex">
-          <span className="material-symbols-outlined -ml-2">login</span>
-          <p className="text-base ml-2">Continue</p>
+    <div>
+      <div>
+        <h2>Log In As Existing User</h2>
+        <div className="grid gap-6 mt-4 text-base">
+          <input
+            className="text-input "
+            type="text"
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <input
+            className="text-input"
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Password"
+          />
         </div>
-      </button>
-    </>
+
+        <button
+          className="bg-green-500 text-white py-2 px-4 rounded-md text-base mt-6 hover:bg-green-600"
+          onClick={handleLogin}
+        >
+          <div className="flex">
+            <span className="material-symbols-outlined -ml-2">login</span>
+            <p className="text-base ml-2">Continue</p>
+          </div>
+        </button>
+      </div>
+      <div>
+        <h2>Create A New Account</h2>
+        <div className="grid gap-6 mt-4 text-base">
+          <input
+            className="text-input "
+            type="text"
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <input
+            className="text-input"
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Password"
+          />
+        </div>
+
+        <button
+          className="bg-green-500 text-white py-2 px-4 rounded-md text-base mt-6 hover:bg-green-600"
+          onClick={handleSignup}
+        >
+          <div className="flex">
+            <span className="material-symbols-outlined -ml-2">login</span>
+            <p className="text-base ml-2">Continue</p>
+          </div>
+        </button>
+      </div>
+    </div>
   );
 }
