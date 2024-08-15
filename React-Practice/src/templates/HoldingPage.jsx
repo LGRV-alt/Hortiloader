d568d9cca537a22e52bb37b3c2f914188a4f2158
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2024-08-15 16:08:18 +0100

    fixed sizing on the elements in the holding page

--- React-Practice/src/templates/HoldingPage.jsx
+++ React-Practice/src/templates/HoldingPage.jsx
@@ -9,27 +9,28 @@ export default function HoldingPage({ records }) {
 
   return (
     <div>
-      <CreateHolding></CreateHolding>
+      <div className="hidden md:block">
+        <CreateHolding></CreateHolding>
+      </div>
       <div className="flex justify-start flex-col mx-5 mt-5 ">
         {holding.map((record) => (
           <div className="flex  items-center border-b-2 mb-5" key={record.id}>
-            <Link to={`/edit/${record.id}`}>
-              <button className="ml-4 bg-gray-500 rounded-md text-white px-2 hover:bg-gray-600 mr-2">
-                <span className="">edit</span>
-              </button>
-            </Link>
             <button
               className="mr-2 bg-red-500 rounded-md text-white px-2 hover:bg-red-600"
               onClick={() => deleteTask(record.id)}
             >
               <span className="material-symbols-outlined">X</span>
             </button>
-            <h3 className="font-medium text-lg mr-2">{record.title}</h3>
-            <p className="font-medium text-lg mr-2">
-              {record.postcode.toUpperCase()}
-            </p>
-            <p className="mr-2">{record.orderNumber}</p>
-            <p>{record.orderInfo}</p>
+            <Link to={`/edit/${record.id}`}>
+              <div className="flex ">
+                <h3 className="font-medium md:text-lg mr-2">{record.title}</h3>
+                <p className="font-medium md:text-lg mr-2">
+                  {record.postcode.toUpperCase()}
+                </p>
+                <p className="mr-2 ">{record.orderNumber}</p>
+                <p className="hidden md:block">{record.orderInfo}</p>
+              </div>
+            </Link>
           </div>
         ))}
       </div>
