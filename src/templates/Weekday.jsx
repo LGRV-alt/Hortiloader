15a2adeeb7b538ac5991503601a5e8854fc54e41
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2025-06-08 13:28:39 +0100

    finished style changes to the weekdays page

--- src/templates/Weekday.jsx
+++ src/templates/Weekday.jsx
@@ -64,18 +64,18 @@ export default function WeekdayPage({ records }) {
         className="m-0 p-0 w-full h-full bg-white"
         style={{ margin: 0, padding: 0 }}
       >
-        <div className="w-full h-36 flex justify-center items-center bg-slate-300 text-center  border-b-2 border-black">
-          <h3 className="text-3xl font-bold">{`${day}-${number} ${year}`}</h3>
+        <div className="w-full md:h-36 h-16 flex justify-center items-center bg-slate-300 text-center  border-b-2 border-black">
+          <h3 className="md:text-3xl text-xl font-bold">{`${day}-${number} ${year}`}</h3>
         </div>
 
         <div className="flex flex-col justify-center ">
           {arr.map((record) => (
             <div
               key={record.id}
-              className="flex justify-between items-center border-b-2 p-3 border-black pl-16 "
+              className="flex justify-between items-center border-b-2 p-3 border-black  md:pl-16 "
             >
               <Link to={`/edit/${record.id}`}>
-                <div className="flex hover:border-black hover:border-b-2 gap-4">
+                <div className="flex hover:border-black hover:border-b-2 gap-1 md:gap-4">
                   <p
                     className={`font-normal md:text-4xl ${
                       record.customerType === "retail"
@@ -89,17 +89,17 @@ export default function WeekdayPage({ records }) {
                   >
                     {record.title}
                   </p>
-                  <p className="text-3xl self-end">
+                  <p className="md:text-3xl text-xs  self-end">
                     {record.orderNumber || ""}
                   </p>
-                  <p className="text-3xl self-end">
+                  <p className="md:text-3xl text-xs  self-end">
                     {record.postcode.toUpperCase() || ""}
                   </p>
                 </div>
               </Link>
 
               {extras && (
-                <div className="text-2xl flex gap-4 pr-8">
+                <div className="md:text-2xl text-xs flex gap-1 md:gap-4 md:pr-8">
                   {[
                     "Green",
                     "Yellow",
@@ -110,7 +110,7 @@ export default function WeekdayPage({ records }) {
                   ].map((label) => (
                     <div key={label} className="flex gap-1">
                       <p className="pb-2">{label}</p>
-                      <span className="self-end w-20 h-14 border-black border-2"></span>
+                      <span className="self-end w-6 h-6 md:w-20 md:h-14 border-black border-2"></span>
                     </div>
                   ))}
                 </div>
@@ -126,7 +126,7 @@ export default function WeekdayPage({ records }) {
         )}
       </div>
 
-      <div className="pb-4 w-full flex justify-center">
+      <div className="pb-4 pt-2 w-full flex justify-center">
         <button
           className="w-1/6 mr-2 px-6 py-2 bg-orange-600 text-white font-semibold rounded-lg shadow hover:bg-orange-700 transition"
           onClick={() => setExtras(!extras)}
