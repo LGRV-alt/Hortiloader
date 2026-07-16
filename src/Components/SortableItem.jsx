a476a2b2d20347fc1c7a13ab12d803c247d89969
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2026-07-16 20:24:32 +0100

    Trolley Mapper
    Working on adding extra info into the page

--- src/Components/SortableItem.jsx
+++ src/Components/SortableItem.jsx
@@ -37,6 +37,7 @@ export default function SortableItem({
       orderNumber: item.orderNumber || "",
       trollies: item.trollies || "",
       extras: item.extras || "",
+      orderInfo: item.orderInfo || "",
     });
   }, [isEditing, item]);
 
@@ -83,115 +84,132 @@ export default function SortableItem({
       }
     >
       {isEditing ? (
-        <div className="p-2 gap-2 flex items-center text-xs md:text-base ">
-          <span
-            {...attributes}
-            {...listeners}
-            className="cursor-grab select-none touch-none text-gray-500 dark:text-white"
-            title="Drag"
-          >
-            {/* <GripVertical size={16} /> */}
-            {index + 1}.
-          </span>
-          <input
-            type="text"
-            className="w-2/3 border rounded text-center border-borderDark dark:bg-slate-500"
-            value={formData.title}
-            onChange={(e) => handleChange("title", e.target.value)}
-            placeholder="Title"
-          />
-          <input
-            type="text"
-            className="w-1/3 border rounded text-center border-borderDark dark:bg-slate-500"
-            value={formData.orderNumber}
-            onChange={(e) => handleChange("orderNumber", e.target.value)}
-            placeholder="Order No"
-          />
-          <input
-            type="text"
-            className="w-1/3 border rounded text-center border-borderDark dark:bg-slate-500"
-            value={formData.postcode}
-            onChange={(e) => handleChange("postcode", e.target.value)}
-            placeholder="Postcode"
-          />
-          <input
-            type="text"
-            className="w-1/3 border rounded text-center border-borderDark dark:bg-slate-500"
-            value={formData.trollies}
-            onChange={(e) => handleChange("trollies", e.target.value)}
-            placeholder="Trollies"
-          />
-          <input
-            type="text"
-            className="w-full border rounded text-center border-borderDark dark:bg-slate-500"
-            value={formData.extras}
-            onChange={(e) => handleChange("extras", e.target.value)}
-            placeholder="Extras"
-          />
-          <button
-            onClick={() => onDelete(item.id)}
-            className="text-red-600 hover:text-red-800 text-sm"
-          >
-            X
-          </button>
-        </div>
-      ) : (
-        <div className="flex justify-between items-center ">
-          <div className="flex items-center gap-2">
+        <div className=" flex flex-col items-center text-xs md:text-base ">
+          <div className="flex gap-2 p-2">
             <span
               {...attributes}
               {...listeners}
-              className="cursor-grab select-none touch-none md:text-xl"
+              className="cursor-grab select-none touch-none text-gray-500 dark:text-white"
               title="Drag"
             >
               {/* <GripVertical size={16} /> */}
               {index + 1}.
             </span>
-            <div
-              className={`flex gap-2 capitalize text-sm items-center font-normal capitalize  ${
-                item.customerType === "retail"
-                  ? "text-blue-700"
-                  : item.customerType === "other"
-                    ? "text-red-500"
-                    : item.customerType === "missed"
-                      ? "text-fuchsia-600"
-                      : ""
-              }`}
+            <input
+              type="text"
+              className="w-2/3 border rounded text-center border-borderDark dark:bg-slate-500"
+              value={formData.title}
+              onChange={(e) => handleChange("title", e.target.value)}
+              placeholder="Title"
+            />
+            <input
+              type="text"
+              className="w-1/3 border rounded text-center border-borderDark dark:bg-slate-500"
+              value={formData.orderNumber}
+              onChange={(e) => handleChange("orderNumber", e.target.value)}
+              placeholder="Order No"
+            />
+            <input
+              type="text"
+              className="w-1/3 border rounded text-center border-borderDark dark:bg-slate-500"
+              value={formData.postcode}
+              onChange={(e) => handleChange("postcode", e.target.value)}
+              placeholder="Postcode"
+            />
+            <input
+              type="text"
+              className="w-1/3 border rounded text-center border-borderDark dark:bg-slate-500"
+              value={formData.trollies}
+              onChange={(e) => handleChange("trollies", e.target.value)}
+              placeholder="Trollies"
+            />
+            <input
+              type="text"
+              className="w-full border rounded text-center border-borderDark dark:bg-slate-500"
+              value={formData.extras}
+              onChange={(e) => handleChange("extras", e.target.value)}
+              placeholder="Extras"
+            />
+            <button
+              onClick={() => onDelete(item.id)}
+              className="text-red-600 hover:text-red-800 text-sm"
             >
-              <p className="font-semibold md:text-2xl">
-                {item.title.toLowerCase()}
-                {console.log(item)}
-              </p>
-              <p className="text-xs md:text-xl">
-                {reduceOrderNumber(item.orderNumber)}
-              </p>
-              <p className="">{sortPostCode(item.postcode)}</p>
-            </div>
+              X
+            </button>
           </div>
-          <div className="flex items-center gap-2 text-sm md:text-2xl">
-            {item.trollies && <p>{item.trollies}T</p>}
-            {item.extras && <p className="text-red-500">{item.extras}</p>}
-          </div>
-        </div>
-      )}
-      {item.orderInfo.length > 1 ? (
-        <div
-          className={`flex gap-2 ${toggleInfo === true ? "" : "print:hidden"}`}
-        >
+
           <input
-            type="checkbox"
-            className="print:hidden"
-            checked={toggleInfo}
-            onChange={(e) => setToggleInfo(e.target.checked)}
+            type="text"
+            className="w-3/4 border rounded text-center border-borderDark dark:bg-slate-500"
+            value={formData.orderInfo}
+            onChange={(e) => handleChange("orderInfo", e.target.value)}
+            placeholder="Order Info"
           />
-          {toggleInfo !== false ? (
-            <p className="">{item.orderInfo}</p>
-          ) : (
-            <p className="text-gray-400 print:hidden">{item.orderInfo}</p>
-          )}
         </div>
       ) : (
-        ""
+        // Task Holder
+        <div className="flex flex-col gap-2 w-full ">
+          {/* Task Information */}
+          <div className="flex justify-between items-center">
+            <div className="flex gap-2">
+              <span
+                {...attributes}
+                {...listeners}
+                className="cursor-grab select-none touch-none md:text-xl"
+                title="Drag"
+              >
+                {/* <GripVertical size={16} /> */}
+                {index + 1}.
+              </span>
+              <div
+                className={`flex gap-2 capitalize text-sm items-center font-normal capitalize  ${
+                  item.customerType === "retail"
+                    ? "text-blue-700"
+                    : item.customerType === "other"
+                      ? "text-red-500"
+                      : item.customerType === "missed"
+                        ? "text-fuchsia-600"
+                        : ""
+                }`}
+              >
+                <p className="font-semibold md:text-2xl">
+                  {item.title.toLowerCase()}
+                  {console.log(item)}
+                </p>
+                <p className="text-xs md:text-xl">
+                  {reduceOrderNumber(item.orderNumber)}
+                </p>
+                <p className="">{sortPostCode(item.postcode)}</p>
+              </div>
+              {/* Trolley Count + Extras */}
+            </div>
+            <div className="flex items-center gap-2 text-sm md:text-2xl">
+              {item.trollies && <p>{item.trollies}T</p>}
+              {item.extras && <p className="text-red-500">{item.extras}</p>}
+            </div>
+          </div>
+
+          {/* Other Information */}
+          {item.orderInfo.length > 1 ? (
+            <div
+              className={`flex gap-2 ${toggleInfo === true ? "" : "print:hidden"}`}
+            >
+              <input
+                type="checkbox"
+                className="print:hidden"
+                checked={toggleInfo}
+                onChange={(e) => setToggleInfo(e.target.checked)}
+              />
+              {toggleInfo !== false ? (
+                <p className="">{item.orderInfo}</p>
+              ) : (
+                <p className="text-gray-400 print:hidden">{item.orderInfo}</p>
+              )}
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
       )}
     </li>
   );
